03ea03cd70d8ce8f2cb7c98e8d843b1a8f805987
DaftDuck2000 <rowanchrimes09@gmail.com>
2024-04-25 19:33:36 +0100

    V1 getting files from user

--- Main.py
+++ Main.py
@@ -1,2 +1,28 @@
 import socket
 
+def main():
+    host = "0.0.0.0"
+    port = 8000
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    
+    print("Server listening on port", port)
+    
+    while True:
+        client_socket, addr = server_socket.accept()
+        print("Connection from:", addr)
+        
+        with open("received_file.txt", "wb") as file:
+            while True:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                file.write(data)
+                
+        print("File received successfully")
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
